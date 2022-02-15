b86c9afcd8a9044bf070ff098799c2028168f715
SnO2WMaN <me@sno2wman.net>
2022-02-15 12:36:48 +0900

    refactoring

--- src/edit/edit.ts
+++ src/edit/edit.ts
@@ -1,5 +1,6 @@
 import { channel } from "../main.ts";
 import { updateView } from "../view/mod.ts";
+import { CommitUnion, Line } from "../types.ts";
 
 export const setupEdit = async () => {
   await channel.declareExchange({ exchange: "edit", type: "topic", durable: true });
@@ -9,54 +10,43 @@ export const setupEdit = async () => {
   await channel.consume(
     { queue: "edit" },
     async (args, props, data) => {
-      const { documentId, lines, commits } = JSON.parse(new TextDecoder().decode(data));
-      const { lines: newLines } = build({ documentId, lines, commits });
-      await updateView(documentId, { lines: newLines });
+      const { documentId, lines: previousLines, commits } = JSON.parse(new TextDecoder().decode(data));
+      const { lines: nextLines } = build(previousLines, commits);
+      await updateView(documentId, { lines: nextLines });
       await channel.ack({ deliveryTag: args.deliveryTag });
     },
   );
 };
 
-const build = ({ documentId, lines, commits }: {
-  documentId: string;
-  lines: { id: string; text: string }[];
-  commits: (
-    & { id: string }
-    & (
-      | { type: "UPDATE"; payload: { lineId: string; text: string } }
-      | { type: "INSERT"; payload: { prevLineId: string; newLineId: string; text: string } }
-      | { type: "DELETE"; payload: { lineId: string } }
-    )
-  )[];
-}) => {
-  let parsed = lines;
+const build = (previousLines: Line[], commits: CommitUnion[]) => {
+  let nextLines = previousLines;
   let head: string | undefined = undefined;
   for (const commit of commits) {
     switch (commit.type) {
       case "UPDATE": {
         const { payload } = commit;
-        parsed = parsed.map((line) => line.id === payload.lineId ? { ...line, text: payload.text } : line);
+        nextLines = nextLines.map((line) => line.id === payload.lineId ? { ...line, text: payload.text } : line);
         head = commit.id;
         break;
       }
       case "INSERT": {
         const { payload } = commit;
-        const index = parsed.findIndex((line) => line.id === payload.prevLineId);
+        const index = nextLines.findIndex((line) => line.id === payload.prevLineId);
         if (index !== -1) {
-          parsed = [
-            ...parsed.slice(0, index + 1),
+          nextLines = [
+            ...nextLines.slice(0, index + 1),
             { id: payload.newLineId, text: payload.text },
-            ...parsed.slice(index + 1),
+            ...nextLines.slice(index + 1),
           ];
           head = commit.id;
         }
         break;
       }
       case "DELETE": {
-        parsed = parsed;
+        nextLines = nextLines;
         break;
       }
     }
   }
-  return { lines };
+  return { lines: nextLines };
 };
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,10 @@
-export type Doc = { lines: { id: string; text: string }[] };
+export type Line = { id: string; text: string };
+export type Doc = { lines: Line[] };
+export type CommitUnion = (
+  & { id: string }
+  & (
+    | { type: "UPDATE"; payload: { lineId: string; text: string } }
+    | { type: "INSERT"; payload: { prevLineId: string; newLineId: string; text: string } }
+    | { type: "DELETE"; payload: { lineId: string } }
+  )
+);
